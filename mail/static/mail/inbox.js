91773d4f82756f01670303505bbfb7bf730ebb5c
AmirAbbas Mashayekhi <AmirAbbasMashayekhi.dev@gmail.com>
2024-10-15 00:58:58 +0330

    Add compose and send functionality

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,10 +20,40 @@ function compose_email() {
   document.querySelector("#emails-view").style.display = "none";
   document.querySelector("#compose-view").style.display = "block";
 
-  // Clear out composition fields
-  document.querySelector("#compose-recipients").value = "";
-  document.querySelector("#compose-subject").value = "";
-  document.querySelector("#compose-body").value = "";
+  // Clear form fields
+  clearComposeFields();
+
+  // Form
+  const composeForm = document.querySelector("#compose-form");
+  // Add event listener for submitting the form
+  composeForm.onsubmit = (event) => {
+    event.preventDefault();
+    // Get the submitted data from the form and POST it to the /emails route
+    fetch("/emails", {
+      method: "POST",
+      body: JSON.stringify({
+        recipients: document.querySelector("#compose-recipients").value,
+        subject: document.querySelector("#compose-subject").value,
+        body: document.querySelector("#compose-body").value,
+      }),
+    })
+      .then((response) =>
+        response
+          .json()
+          .then((data) => ({ status: response.status, body: data }))
+      )
+      .then(({ status, body }) => {
+        if (status === 201) {
+          load_mailbox("sent");
+        } else if (status === 400) {
+          clearComposeFields();
+          showError(body.error);
+        }
+      })
+      .catch((error) => {
+        console.error("Error: ", error);
+      });
+  };
 }
 
 function load_mailbox(mailbox) {
@@ -35,4 +65,49 @@ function load_mailbox(mailbox) {
   document.querySelector("#emails-view").innerHTML = `<h3>${
     mailbox.charAt(0).toUpperCase() + mailbox.slice(1)
   }</h3>`;
+
+  // Send GET request to the server to get the list of emails in that mailbox
+  fetch(`/emails/${mailbox}`)
+    // Convert the response to json
+    .then((response) => response.json())
+    // For each mail in the response create a UI representation
+    .then((emails) => {
+      emails.forEach((email) => {
+        const emailUI = document.createElement("div");
+        emailUI.classList.add("card");
+        // --- TODO
+        // Get the parent element for these email UI's
+        const emailsView = document.querySelector("#emails-view");
+        emailsView.appendChild(emailUI);
+      });
+    });
+}
+
+function showError(errorMessage) {
+  // Remove previous elements
+  removeElementIfExists("#compose-error");
+
+  // Create a div for displaying error messages
+  const errorDiv = document.createElement("div");
+  errorDiv.id = "compose-error";
+  errorDiv.className = "alert alert-danger";
+  errorDiv.innerText = errorMessage;
+
+  // Insert the error div above the form or below the "To" input
+  const form = document.querySelector("#compose-form");
+  form.prepend(errorDiv);
+}
+
+function clearComposeFields() {
+  // Clear out composition fields
+  document.querySelector("#compose-recipients").value = "";
+  document.querySelector("#compose-subject").value = "";
+  document.querySelector("#compose-body").value = "";
+}
+
+function removeElementIfExists(selector) {
+  const element = document.querySelector(selector);
+  if (element) {
+    element.remove();
+  }
 }
